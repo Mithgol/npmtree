544c104a002c8694dbea68bb7534b5600bab18ad
Mithgol the Webmaster <getgit@mithgol.ru>
2015-04-30 12:16:28 +0300

    explain the CLI argument (`packageName`) if called without it

--- npmtree.js
+++ npmtree.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
-var npmtree = require('./treecore.js');
+var clog = console.log;
 
-npmtree();
+if( process.argv.length < 3 ){
+   clog('Usage:');
+   clog('   npmtree packageName');
+   clog('');
+   clog('The parameter packageName is a name of a package in npm repository');
+   clog('(may also contain its version and other npm-accepted details).');
+   clog('');
+   clog('Examples:');
+   clog('   npmtree uue');
+   clog('   npmtree twi2fido@1.0.3');
+} else clog( require('./treecore.js')(process.argv[2]) );
